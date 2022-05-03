3a01608126e5e09ccde733bb7658780c24d3208e
Aya Ramirez <ayaramz@gmail.com>
2022-05-03 18:35:42 -0400

    commit tree b35b8f50a7418830a6c90a2fbe9cad71beb98766

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-function addingEventListener() {
+
+
+ function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function(){
+        alert('I was CLICKED!!!!!!!!!');
+    })
 }
+
+//1. the name of the event to listen for, and
+//2. a _callback function_ to "handle" the event
